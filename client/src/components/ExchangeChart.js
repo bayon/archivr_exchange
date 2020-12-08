d44079efa49a54010ffaa8b50e10f3c0d6b69a06
Bayon Forte <bayon@forteworks.com>
2020-12-07 21:32:05 -0500

    got the data onto the chart.

--- client/src/components/ExchangeChart.js
+++ client/src/components/ExchangeChart.js
@@ -5,25 +5,53 @@ function ExchangeChart(props) {
   //const [chartData, setChartData] = useState({});
 
    console.log("Exchange Chart props:",props)
+   console.log("type:")
+   console.log(typeof props)
+   console.log("type:")
+   console.log(typeof props.currentChartData)
+   const roughObj = props.currentChartData;
+   console.log(Array.isArray(roughObj))
+   console.log(roughObj);
+
+   var _labels = [];
+   var _dataset1 = [];
+   var _dataset2 = [];
+   var _curr1 = ""
+   var _curr2 = ""
+
+    for(var i in roughObj){
+       console.log("loop i:",i)
+       let obj = roughObj[i]
+       console.log(obj) 
+       _labels.push(obj.key) 
+        
+       var valuesObj = obj.value;
+       var valArray = Object.values(valuesObj)
+       _dataset1.push(valArray[0]);
+       _dataset2.push(valArray[1])
+
+       var keyArray = Object.keys(valuesObj) 
+       _curr1 = keyArray[0];
+       _curr2 = keyArray[1];
+
+
+    }
+
+
+   
 
   const chartData = {
       // labels will be dates.
-    labels: [
-      '2020-11-01',
-      '2020-11-05',
-      '2020-11-27',
-      '2020-12-01',
-      '2020-12-23',
-      '2020-12-26',
-    ],
+    labels: _labels ,
+   
     datasets: [
       {
-        label: 'Currency One',
-        data: [1.33, 1.30, 1.1, 1.0, 1.1, 1.2],
+        label: _curr1,
+        data: _dataset1, 
       },
       {
-        label: 'Currency Two',
-        data: [.85, .95, .85, .83, .82, .95],
+        label: _curr2,
+        data: _dataset2, 
       },
     ],
   }
